3d2cab0630e5603060561bd9760e59074dfc8d22
Leandro Segovia <ldlsegovia@gmail.com>
2016-07-22 18:09:47 -0300

    Wip

---
+++ src/app/rut/rut.pipe.spec.ts
@@ -0,0 +1,11 @@
+// /* tslint:disable:no-unused-variable */
+//
+// import { addProviders, async, inject } from '@angular/core/testing';
+// import { RutPipe } from './rut.pipe';
+//
+// describe('Pipe: Rut', () => {
+//   it('create an instance', () => {
+//     let pipe = new RutPipe();
+//     expect(pipe).toBeTruthy();
+//   });
+// });
